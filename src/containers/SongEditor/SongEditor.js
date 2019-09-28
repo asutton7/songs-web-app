d7cd8919fd23e05bc7bf70092fbb83893406c5df
Aaron Sutton <aaronsutton82@gmail.com>
2019-09-28 12:48:33 -0500

    We added redux and Ionic to the project
    Please enter the commit message for your changes. Lines starting

--- src/containers/SongEditor/SongEditor.js
+++ src/containers/SongEditor/SongEditor.js
@@ -3,12 +3,9 @@ import firebase from '../../firebase';
 import classes from './SongEditor.module.css';
 import Record from '@material-ui/icons/FiberManualRecord'
 import Play from '@material-ui/icons/PlayArrow'
+import {connect} from 'react-redux';
 
 class SongEditor extends Component {
-    constructor(props){
-        super(props);
-    }
-    
     state = {
         lyrics: '',
         title: this.props.match.params.title
@@ -22,7 +19,9 @@ class SongEditor extends Component {
             lyrics: null,
             title: null
         };
-        db.collection("users/" +firebase.auth().currentUser.email+ "/songs/").doc(this.props.match.params.id)
+        console.log("users/" +firebase.auth().currentUser.email+ "/songs/"+this.props.openFolderKeys.join('')+this.props.match.params.id)
+        db.collection("users/" +firebase.auth().currentUser.email+ "/songs/"+this.props.openFolderKeys.join(''))
+            .doc(this.props.match.params.id)
             .get()
             .then((doc) => {
                 song.id = doc.id;
@@ -61,4 +60,11 @@ class SongEditor extends Component {
     }
 }
 
-export default SongEditor;
+const mapStateToProps = state => {
+    return {
+        openFolderKeys: state.openFolderKeys,
+    };
+}
+
+
+export default connect(mapStateToProps)(SongEditor);
--- src/index.js
+++ src/index.js
@@ -3,8 +3,31 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {Provider} from 'react-redux';
+import { createStore, applyMiddleware, compose} from 'redux';
+import thunk from 'redux-thunk';
+import reducer from './store/reducer';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const logger = store => {
+    return next => {
+        return action => {
+            console.log('middleware dispatching', action);
+            const result = next(action);
+            console.log('next state', store.getState());
+            return result;
+        }
+    }
+};
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducer, composeEnhancers(applyMiddleware(logger, thunk)));
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
